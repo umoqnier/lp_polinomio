0eaee4b45b8877ddb3ffb9b1511e97aaa3f1cbb5
Diego Alberto Barriga Martínez <umoqnier@gmail.com>
2016-08-17 00:41:46 -0500

    Modificaciones Mayores
    Cambio de la estructura del programa, se agregaron funciones para cada caso condicional y una funcion main.
    Solo funciona en entornos Windows.
    Funcionando casos: 1, 2, 3, 5

--- polinomio.py
+++ polinomio.py
@@ -1,4 +1,4 @@
-#Universidad Nacioanal Autonoma de Mexico/ Facultad de Ingenieria
+#Universidad Nacional Autonoma de Mexico/ Facultad de Ingenieria
 #Lenguajes de programacion
 #Alumnos:
 #-Banda Martínez César Eduardo
@@ -8,106 +8,152 @@
 #Hecho en Python 3.6
 
 #Inicio de programa
+#Importando modulos
+import os
 #Variables globales
 i=0
 j=0
 l=0
 k=0
+sentinela = 1
 m=1
 resultado=0
+
 def grado():
-	grado=int(input("Introduce el grado maximo: "))
+	grado = int(input("Introduce el grado maximo del polinomio: "))
 	return grado
 
-def polinomioUno():
+def crea_polinomio():
 	i=0
 	grado1 = grado()
-	polinomioUno= []
-	while i<grado1+1:
-		valor=int(input(("Inserta el valor del coeficiente para x^"+str(i)+"...")))
-		polinomioUno.insert(i,valor)
+	polinomio= []
+	while i < grado1+1:
+		coeficiente = int(input(("Inserta el valor del coeficiente para x^"+str(i)+": ")))
+		polinomio.append(coeficiente)
 		i=i+1
-	print ("Los valores del polinomio 1 introducido son: ")
-	for l in range(len(polinomioUno)):
-		print(str(polinomioUno[l])+"x^"+str(l),)
-	return polinomioUno
+	return polinomio
 
-def polinomioDos():
-	i=0
-	grado2=grado()
-	polinomioDos= []
-	while i<grado2+1:
-		valor=int(input(("Inserta el valor del coeficiente para x^"+str(i)+"...")))
-		polinomioDos.insert(i,valor)
-		i=i+1
-	print ("Los valores del polinomio 2 introducido son: ")
-	for l in range(len(polinomioDos)):
-		print(str(polinomioDos[l])+"x^"+str(l),)
-	return polinomioDos
-
-print("""
-		1-Valor calculado en un punto
-		2-Suma
-		3-Resta
-		4-Igualdad de 2 polinomios
-		5-Polinomio opuesto
-		6-Multiplicacion
-
-		""")
-opcion=int(input("Escoge una opcion: "))
-
-#Valor en un punto
-if opcion==1: 
-	polinomio=polinomioUno();
-	print ("Los valores del polinomio introducido son: ")
-	for l in range(len(polinomio)):
-		print(str(polinomio[l])+"x^"+str(l),)
-
-	punto = int(input("Ingresa el valor del punto: "))
+def valor_punto(polinomio1):
+	print("==============Valor calculado en un punto==============\n")
+	punto = int(input("Ingresa el valor de x: "))
 	z=0
 	potencias=[]
-	for j in range(0,len(polinomio)):
+	for j in range(0, len(polinomio1)):
 		potencias.insert(j,pow(punto,z))
 		z=z+1
 	multiplicacion = 0
-	for k in range(0,len(polinomio)):
-		multiplicacion = multiplicacion+(polinomio[k]*potencias[k])
+
+	for k in range(0,len(polinomio1)):
+		multiplicacion = multiplicacion + (polinomio1[k]*potencias[k])
 		k=k+1
 	print("El valor calculado es: "+str(multiplicacion))
+	print("")
 
-#Suma de dos polinomios
-elif opcion==2:
+def suma_polinomios(polinomio1):
+	print("==============Suma de Polinomios==============\n")
 	print("Los polinomios deben ser del mismo grado")
 	suma=[]
-	polinomio1= polinomioUno()
-	polinomio2= polinomioDos()
-	for i in range(0,len(polinomio1)):
-		suma.append(polinomio1[i]+polinomio2[i])
-	print("El polinomio resultante es: ")
-	for j in range(0,len(suma)):
-		print(str(suma[j])+"x^"+str(j),),
+	polinomio_aux = crea_polinomio()
+	for i in range(0, len(polinomio1)):
+		suma.append(polinomio1[i]+polinomio_aux[i])
 
-#Resta de polinomios
-elif opcion==3:
+	print("\t\tEl polinomio resultante es: ", end="")
+	for j in range(0, len(suma)):
+		print("+("+str(suma[j])+")"+"x^"+str(j), end="")
+	print("")
+
+	return suma
+
+def resta_polinomios(polinomio1):
+	print("==============Resta de Polinomios==============\n")
 	print("Los polinomios deben ser del mismo grado")
 	resta=[]
-	polinomio1= polinomioUno()
-	polinomio2= polinomioDos()
-	for i in range(0,len(polinomio1)):
-		resta.append(polinomio1[i]-polinomio2[i])
-	print("El polinomio resultante es: ")
+	polinomio_aux = crea_polinomio()
+	for i in range(0, len(polinomio1)):
+		resta.append(polinomio1[i]-polinomio_aux[i])
+
+	print("El polinomio resultante es: ", end="")
 	for j in range(0,len(resta)):
-		print(str(resta[j])-"x^"-str(j),),
+		print("+("+str(resta[j])+")"+"x^"+str(j), end = "")
+	print("")
+
+	return resta
+
+def polinomio_opuesto(polinomio1):
+	print("==============Polinomios opuestos==============\n")
+	print("NOTA: Los polinomios deben ser del mismo grado")
+	opuesto = []
+	polinomio_aux = crea_polinomio()
+
+	for i in range(0, len(polinomio_aux)):
+		opuesto.append(-(polinomio_aux[i]))
+
+	if polinomio1 == opuesto:
+		print("Los polinomios son opuestos")
+	else:
+		print("Los polinomios no son opuestos")
+
+def multiplica_polinomios(polinomio1):
+	"""NO FUNCIONA CORRECTAMENTE"""
+	print("==============Multiplicacion de Polinomios==============\n")
+	producto = []
+	polinomio_aux = crea_polinomio()
+	for i in range(0, len(polinomio1)):
+		producto.append(polinomio1[i]*polinomio_aux[i])
+
+	print("El polinomio resultante es: ", end="")
+	for j in range(0,len(producto)):
+		print("+("+str(producto[j])+")"+"x^"+str(j), end = "")
+	print("")
+
+	return producto
 	
-#Igualdad de polinomios
-elif opcion==4:
-	igualdad=[]
-	polinomio1 = polinomioUno()
-	polinomio2 = polinomioDos()
-	if grado2==grado1:
-	   print("El grado del polinomio es igual")
-	print("El grado del polinomio no es igual")   
+def main(polinomio1):
+	while sentinela != 0:
+		print("""
+			1-Valor calculado en un punto
+			2-Suma
+			3-Resta
+			4-Igualdad de 2 polinomios
+			5-Polinomio opuesto
+			6-Multiplicacion
+			0-Salir
+			""")
 
+		print("\t\tPolinomio: ", end="")
+		for l in range(len(polinomio1)):
+				print("+("+str(polinomio1[l])+")"+"x^"+str(l)+" ", end="")
+		print("")
+		opcion=int(input("Escoge una opcion: "))
 
+		#Valor en un punto
+		if opcion==1:
+			valor_punto(polinomio1)
 
+		#Suma de dos polinomios
+		elif opcion==2:
+			polinomio1 = suma_polinomios(polinomio1)
 
+		#Resta de polinomios
+		elif opcion==3:
+			polinomio1 = resta_polinomios(polinomio1)	
+				
+		#Igualdad de polinomios
+		elif opcion==4:
+			igualdad=[]
+			polinomio1 = polinomioUno()
+			polinomio2 = polinomioDos()
+			if grado2 == grado1:
+			   print("El grado del polinomio es igual")
+			print("El grado del polinomio no es igual")
+		elif opcion == 5:
+			polinomio_opuesto(polinomio1)
+		elif opcion == 6:
+			polinomio1 = multiplica_polinomios(polinomio1)
+		elif opcion == 0:
+			break
+		else:
+			print("*Error* Intenta otra opcion *Error*")
+
+os.system("CLS")
+main(crea_polinomio())
